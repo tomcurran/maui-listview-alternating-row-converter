ac3a3b78c9158da2fef25c1831d8cf167ac61713
Tom Curran <tomcurran5@gmail.com>
2025-09-02 12:46:07 +0100

    Added MonkeysPage sample

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,6 +7,7 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+		BindingContext = new MainPageViewModel();
 	}
 
 	private void OnCounterClicked(object? sender, EventArgs e)
---
+++ Monkey.cs
@@ -0,0 +1,10 @@
+namespace maui_listview_alternating_row_converter;
+
+public class Monkey
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Details { get; set; }
+    public string ImageUrl { get; set; }
+    public bool? IsMischievous { get; set; }
+}
